74da1c032667f4ab174f704919242e7ffa13c089
Daniel Mendoza <daniel.mendozateixeira@gmail.com>
2023-03-22 10:02:31 -0300

    comando /elo agora exibe retorna uma imagem

--- commands/elo.js
+++ commands/elo.js
@@ -1,7 +1,8 @@
 const config = require('../config.json');
 let LeagueAPI = require('leagueapiwrapper');
-const {SlashCommands, SlashCommandBuilder} = require('discord.js');
-
+const { AttachmentBuilder, SlashCommands, SlashCommandBuilder } = require('discord.js');
+const Canvas = require('@napi-rs/canvas');
+const { readFile } = require('fs/promises');
 LeagueAPI = new LeagueAPI(config.leagueAPIKey, Region.BR1);
 
 module.exports = {
@@ -9,31 +10,77 @@ module.exports = {
     .setName('elo')
     .setDescription('Elo do jogador')
     .addStringOption(option => option.setName("nickname").setDescription("Nick do jogador")),
-  async execute(interaction){
-    var msgResposta = "";
-    const summonerName = interaction.options.getString('nickname');
-    const summoner = await LeagueAPI.getSummonerByName(summonerName, 'br1');
-    const rankingSummoner = await LeagueAPI.getLeagueRanking(summoner);
-    if (rankingSummoner.length > 0) {
-      for (var i in rankingSummoner) {
-        if(rankingSummoner[i].queueType != "RANKED_TFT_DOUBLE_UP"){
-          msgResposta += summoner.name + " - "
-          + rankingSummoner[i].tier + " "
-          + rankingSummoner[i].rank + " "
-          + rankingSummoner[i].leaguePoints + "PDL" + (rankingSummoner[i].queueType === "RANKED_SOLO_5x5" ? " SOLO/DUO " : " FLEX ")
-          + Math.trunc((rankingSummoner[i].wins * 100) / (rankingSummoner[i].losses + rankingSummoner[i].wins)) + "% VIT. "
-          + (rankingSummoner[i].wins + rankingSummoner[i].losses) + " PARTIDAS";
-        msgResposta += rankingSummoner.length > 1 ? " \n" : "";
-        }
-        else{
-          msgResposta = summoner.name + " - Lvl." + summoner.summonerLevel + " UNRANKED";
+  async execute(interaction) {
+    await interaction.deferReply({ ephemeral: true });
+    
+    try {
+      //PLANO DE FUNDO
+      const canvas = Canvas.createCanvas(500, 280);
+      const context = canvas.getContext('2d');
+      const background = await readFile('./assets/wp.png');
+      const backgroundImage = new Canvas.Image();
+      backgroundImage.src = background;
+      context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    
+      const summonerName = interaction.options.getString('nickname');
+      const summoner = await LeagueAPI.getSummonerByName(summonerName, 'br1');
+      const rankingSummoner = await LeagueAPI.getLeagueRanking(summoner);
+
+      //TEXTO NOME JOGADOR
+      context.font = applyText(canvas, summoner.name);
+      context.fillStyle = 'white';
+      context.textAlign = "center";
+      context.fillText(summoner.name, canvas.width / 2, canvas.height / 5);
+      
+      if (rankingSummoner.length > 0) {
+        for (var i in rankingSummoner) {
+          if (rankingSummoner[i].queueType != "RANKED_TFT_DOUBLE_UP") {
+            //INFO DA FILA
+            const emblem = await readFile(`./assets/${rankingSummoner[i].tier.toLowerCase()}.png`);
+            const emblemImage = new Canvas.Image();
+            emblemImage.src = emblem;
+            context.drawImage(emblemImage, canvas.width / 30, canvas.height / (4.2-(i*2.4)), 65, 83);
+
+            context.font = applyText(canvas,`${rankingSummoner[i].tier} ${rankingSummoner[i].rank} ${rankingSummoner[i].leaguePoints}PDL ${Math.trunc((rankingSummoner[i].wins * 100) / (rankingSummoner[i].losses + rankingSummoner[i].wins))}% - ${(rankingSummoner[i].queueType === "RANKED_SOLO_5x5" ? " SOLOQ " : " FLEX ")}`);
+            context.fillStyle = 'white';
+            context.textAlign = "left";
+            context.fillText(`${rankingSummoner[i].tier} ${rankingSummoner[i].rank} ${rankingSummoner[i].leaguePoints}PDL ${Math.trunc((rankingSummoner[i].wins * 100) / (rankingSummoner[i].losses + rankingSummoner[i].wins))}% - ${(rankingSummoner[i].queueType === "RANKED_SOLO_5x5" ? " SOLOQ " : " FLEX ")}`, canvas.width / 5.5, canvas.height / (2.3-(i*1.01)));
+          }
+          else {
+            context.font = applyText(canvas,`${summoner.summonerLevel} Lvl.`);
+            context.fillStyle = 'white';
+            context.textAlign = "center";
+            context.fillText(`${summoner.summonerLevel} Lvl. UNRANKED`, canvas.width / 2, canvas.height / 2);
+          }
         }
       }
+      else {
+        context.font = applyText(canvas,`${summoner.summonerLevel} Lvl.`);
+        context.fillStyle = 'white';
+        context.textAlign = "center";
+        context.fillText(`${summoner.summonerLevel} Lvl. UNRANKED`, canvas.width / 2, canvas.height / 2);
+      }
+      const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'elo.png' });
+      interaction.editReply({ files: [attachment] });
+    } catch (error) {
+      console.log(error);
+      interaction.editReply("Ocorreu um erro ao executar o comando!");
     }
-    else {
-      msgResposta = summoner.name + " - Lvl." + summoner.summonerLevel + " UNRANKED";
-    }
+  },
+};
 
-      await interaction.reply(msgResposta);
-    },
+const applyText = (canvas, text) => {
+  const context = canvas.getContext('2d');
+
+  // Declare a base size of the font
+  let fontSize = 65;
+
+  do {
+    // Assign the font to the context and decrement it so it can be measured again
+    context.font = `${fontSize -= 1}px Bebas Neue`;
+    // Compare pixel width of the text to the canvas minus the approximate avatar size
+  } while (context.measureText(text).width > canvas.width - 100);
+
+  // Return the result to use in the actual canvas
+  return context.font;
 };
